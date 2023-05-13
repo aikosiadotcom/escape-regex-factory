5cb5f30ef6b5174f43d8b307aaa9005e11668a05
firman <dev.aikosia@gmail.com>
2023-05-13 18:25:45 +0700

    docs: change examples

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 if(!RegExp.getInstance){
+    /**
+     * Get escaping instance of RegExp
+     * @param  {...any} args 
+     * @returns {RegExp}
+     */
     RegExp.getInstance = (...args)=>{
         const pattern = args[0];
         args.shift();
